40c3b63f09b1437df3c531628a7c1b3edc279069
berleon <github@leon-sixt.de>
2015-12-18 14:00:34 +0100

    rename test for preprocess

--- test/scripts/test_preprocess.sh
+++ test/scripts/test_preprocess.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-TEST_PATHFILE="test_add_border_pathfile.txt"
+TEST_PATHFILE="test_preprocess_pathfile.txt"
 TEST_IMG=$(realpath ../testdata/Cam_0_20140804152006_3.jpeg)
-WITH_BORDER="/tmp/add_border/Cam_0_20140804152006_3_wb.jpeg"
-OUTPUT_PATHFILE="add_border_pathfile.txt"
+WITH_BORDER="/tmp/preprocess/Cam_0_20140804152006_3_wb.jpeg"
+OUTPUT_PATHFILE="preprocess_pathfile.txt"
 rm -f $WITH_BORDER
 echo "Does $TEST_IMG exists?"
 test -e $TEST_IMG
@@ -13,11 +13,11 @@ test -e $TEST_IMG
 cd ../../source/tagger
 echo $TEST_IMG > $TEST_PATHFILE
 echo "Given a file of image path"
-./add_border -o /tmp/add_border --output-pathfile $OUTPUT_PATHFILE $TEST_PATHFILE
-echo "Then ./add_border will add a border to the image"
+./preprocess -o /tmp/preprocess --output-pathfile $OUTPUT_PATHFILE $TEST_PATHFILE
+echo "Then ./preprocess will add a border to the image"
 test -e $WITH_BORDER
 
-echo "Then ./add_border will create an output pathfile"
+echo "Then ./preprocess will create an output pathfile"
 test -e $OUTPUT_PATHFILE
 
 echo "The output pathfile contains the path to the image with the new border"
